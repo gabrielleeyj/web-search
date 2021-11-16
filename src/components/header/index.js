600e7f4f048e184ccd31912c3368a53c850b1e2d
gabrielleeyj <gabrielleeyj@outlook.com>
2021-11-16 19:56:19 +0800

    update styling of masthead to figma example

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './assets/lion-head-symbol.svg';
+import logo from './../../assets/lion-head-symbol.svg';
 
 const Header = () => {
   return (
-    <div className="bg-gray-100 p-1 flex items-center space-x-2">
-      <div className="ml-4"><img src={logo} alt="Masthead"/></div>
-      <div className="font-sans font-normal text-xs text-gray-600">A Singapore Government Agency Website</div>
+    <div className="bg-gray-100 py-1 px-4">
+      <div className="container mx-auto lg:mx-20 flex items-center space-x-2">
+        <div className="ml-16"><img src={logo} alt="Masthead"/></div>
+        <div className="font-masthead font-normal text-xs text-gray-600">
+          An Official Website of the <strong>Singapore Government</strong>
+        </div>
+      </div>
     </div>
   );
 };
