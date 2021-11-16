36e9acf09c09b3158d66e53257baf64dace1f652
gabrielleeyj <gabrielleeyj@outlook.com>
2021-11-16 19:59:17 +0800

    set default theme settings for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
     fontFamily: {
-      'sans': ['lato'],
+      'masthead': ['Lato'],
     },
     extend: {},
   },
