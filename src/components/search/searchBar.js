030dd54825abfe316bf4f0b222dd2d083a8c3a4a
gabrielleeyj <gabrielleeyj@outlook.com>
2021-11-16 21:20:50 +0800

    adjust position of suggestion

--- src/components/search/searchBar.js
+++ src/components/search/searchBar.js
@@ -12,14 +12,18 @@ const SearchBar = ({ setSearch }) => {
   const [keyword, setKeyword] = useState('');
 
   useEffect(() => {
-    async function fetchData() {
-      // gets base url from axios.js and parse the extension from request
-      const request = await instance.get('/b5544baebde4bfe9fe2d12e8e5502cbf/raw/e026dab444155edf2f52122aefbb80347c68de86/suggestion.json');
-      setSuggestion(request.data.suggestions);
-      console.log(request.data);
+    if (keyword.length > 1) {
+      const fetchData = async () => {
+        // gets base url from axios.js and parse the extension from request
+        const request = await instance.get('/b5544baebde4bfe9fe2d12e8e5502cbf/raw/e026dab444155edf2f52122aefbb80347c68de86/suggestion.json');
+        setSuggestion(request.data.suggestions);
+      }
+      fetchData();
+    } else {
+      setSuggestion([]);
     }
-    fetchData();
-  },[]);
+  },[keyword]);
+
   return (
     <>
     <div className="container mx-auto my-4 px-4 lg:px-20 flex items-center">
@@ -28,14 +32,18 @@ const SearchBar = ({ setSearch }) => {
              value={keyword}
              onChange={(e) => setKeyword(e.target.value)}
       />
+      <ul className="absolute top-28 z-10 flex flex-col flex-grow float-left bg-white shadow-lg border-l border-r border-b border-t-0 border-gray-200">
+        { suggestion ? suggestion.map((val, key)=> (
+          <li key={key} className="pl-8 pr-2 py-1 border-gray-100 cursor-pointer hover:bg-blue-50 hover:text-gray-900">
+            {val}
+          </li>
+        )) : [] }
+      </ul>
       <button className="rounded-lg bg-blue-500 text-white py-3 px-10 flex items-center" onClick={setSearch}>
         <SearchIcon className="h-5 w-5"/>&nbsp;&nbsp;Search
       </button>
     </div>
-      <ul className="container mx-auto bg-white border-l border-r border-b border-t-0 border-gray-200 lg:px-20 -mt-4">
-        {suggestion ? suggestion.map((val, key)=> (
-        <li key={key} className="pl-8 pr-2 py-1 border-gray-100 relative cursor-pointer hover:bg-blue-50 hover:text-gray-900">{val}</li>
-      )) : ''}</ul>
+
 
   </>
 );
